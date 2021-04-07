8f38148612cb643e3cbc921481fe033a80610f93
Moinul Moin <moinulmoin101@gmail.com>
2021-04-08 03:13:54 +0600

    changes 2.0

---
+++ js/script.js
@@ -0,0 +1,13 @@
+        // Mega Menu Script
+    let mainMenu = document.querySelector(".main-menu")
+    let megaMenu = document.querySelector(".mega-menu")
+    mainMenu.addEventListener("mouseover", function () {
+        megaMenu.classList.add("show-mega-menu");
+        mouseleave();
+        })
+
+    function mouseleave() {
+        megaMenu.addEventListener("mouseleave", function () {
+            megaMenu.classList.remove("show-mega-menu");
+        })
+    }
