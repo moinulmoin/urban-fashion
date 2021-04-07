8c44a16416b4650f9a69b0199f1dabebe6c06f1c
Moinul Moin <moinulmoin101@gmail.com>
2021-04-08 03:21:45 +0600

    changes 2.4

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-        // Mega Menu Script
-    let mainMenu = document.querySelector(".main-menu")
-    let megaMenu = document.querySelector(".mega-menu")
-    mainMenu.addEventListener("mouseover", function () {
-        megaMenu.classList.add("show-mega-menu");
-        mouseleave();
-        })
+// Activating  Icons
+feather.replace();
 
-    function mouseleave() {
-        megaMenu.addEventListener("mouseleave", function () {
-            megaMenu.classList.remove("show-mega-menu");
-        })
-    }
+// Mega Menu Script
+let mainMenu = document.querySelector(".main-menu")
+let megaMenu = document.querySelector(".mega-menu")
+mainMenu.addEventListener("mouseover", function () {
+    megaMenu.classList.add("show-mega-menu");
+    mouseleave();
+    })
+function mouseleave() {
+    megaMenu.addEventListener("mouseleave", function () {
+        megaMenu.classList.remove("show-mega-menu");
+    })
+}
